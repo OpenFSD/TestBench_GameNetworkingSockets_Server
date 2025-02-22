889e55e51f69c62603c2bef14bd9144a71636bcd
Full Stack Development Template <Jasper.Assembly@icloud.com>
2025-02-22 18:37:49 +1000

    Add files via upload
    version: DEV_2025_02_22_1834

--- engine/Server.cs
+++ engine/Server.cs
@@ -9,7 +9,8 @@ namespace Server_Assembly
     public class Server
     {
         static private int praiseEventId;
-
+        static private Thread thread_SendResult;
+        static private Thread thread_RecieveInputAction;
         public Server() 
         {
             praiseEventId = 0;
@@ -19,6 +20,26 @@ namespace Server_Assembly
         {
             Valve.Sockets.Library.Initialize();
             Server_Networking.CreateNetworkingServer();
+            thread_SendResult = new Thread(Server_Assembly.Server.Thread_SendResult);
+            thread_SendResult.Start();
+            thread_RecieveInputAction = new Thread(Server_Assembly.Server.Thread_RecieveInputAction);
+            thread_RecieveInputAction.Start();
+        }
+
+        static public void Thread_SendResult()
+        {
+            while (true)
+            {
+                Server_Assembly.Server_Networking.CreateAndSendNewMessage();
+            }
+        }
+
+        static public void Thread_RecieveInputAction()
+        {
+            while (true)
+            {
+                Server_Assembly.Server_Networking.CopyPayloadFromMessage();
+            }
         }
     }
 }
